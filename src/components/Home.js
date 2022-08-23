7f1bf8c7eb729ad232cf49959504ee85ef84fa4f
shamelhok <msh_shamel@hotmail.co.uk>
2022-08-23 12:10:12 +0100

    add articles with pages and individual article pages

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Home(){
+
+    return (<div>
+       <h1>
+        <Link to="/articles"> All articles </Link>
+        </h1> 
+
+    </div>)
+}
