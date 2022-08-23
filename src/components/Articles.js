32b4f440f81b747a276075ffd97661526651b5fe
shamelhok <msh_shamel@hotmail.co.uk>
2022-08-23 14:31:47 +0100

    replace extra h1 tags with h2 and format with prettire

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,46 +1,54 @@
-import { useEffect, useState } from "react"
-import { Link, useLocation, useParams } from "react-router-dom"
-import { fetchArticles } from "../api"
-import ArticleTitleCard from "./ArticleTitleCard"
+import { useEffect, useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { fetchArticles } from "../api";
+import ArticleTitleCard from "./ArticleTitleCard";
 
-export default function Articles(){
-    const params = useParams()
-    const {topic} = params
-    const [currentArticles, setCurrentArticles]= useState([])
-    const [currentPage,setCurrentPage]= useState(1)
-    const [isLoading, setLoading]= useState(true)
-    function decreasePage(){ 
-        if(currentPage>1){
-            
-            setCurrentPage(currentPage=> currentPage-1)}
+export default function Articles() {
+  const params = useParams();
+  const { topic } = params;
+  const [currentArticles, setCurrentArticles] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setLoading] = useState(true);
+  function decreasePage() {
+    if (currentPage > 1) {
+      setCurrentPage((currentPage) => currentPage - 1);
     }
-    function increasePage(){
-       if (currentArticles.length>0){setLoading(true)
-         setCurrentPage(page=> page+1)}
+  }
+  function increasePage() {
+    if (currentArticles.length > 0) {
+      setLoading(true);
+      setCurrentPage((page) => page + 1);
     }
-    useEffect(()=>{
-        fetchArticles({p:currentPage, topic}).then(body => {
-            setCurrentArticles(body.articles)
-        setLoading(false)})
-        .catch(err=>console.log(err))}
-        ,[currentArticles])
+  }
+  useEffect(() => {
+    fetchArticles({ p: currentPage, topic })
+      .then((body) => {
+        setCurrentArticles(body.articles);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [currentArticles]);
 
-    return(<div>
-        {isLoading?<h2> Loading ... </h2>:''}
-        <div>
-            <button onClick={decreasePage}> previous page </button>
-             page {currentPage}
-            <button onClick={increasePage}> next page </button>
-            </div>
-        {currentArticles.map(article=>{
-            return  <Link key={article.title} to={'/articles/'+article.article_id}>
-                <ArticleTitleCard  {...article}/>
-                </Link>
-        })}
-        <div>
-            <button> previous page </button>
-             page {currentPage}
-            <button> next page </button>
-        </div>
-    </div>)
+  return (
+    <div>
+      {isLoading ? <h2> Loading ... </h2> : ""}
+      <div>
+        <button onClick={decreasePage}> previous page </button>
+        page {currentPage}
+        <button onClick={increasePage}> next page </button>
+      </div>
+      {currentArticles.map((article) => {
+        return (
+          <Link key={article.title} to={"/articles/" + article.article_id}>
+            <ArticleTitleCard {...article} />
+          </Link>
+        );
+      })}
+      <div>
+        <button> previous page </button>
+        page {currentPage}
+        <button> next page </button>
+      </div>
+    </div>
+  );
 }
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -2,22 +2,26 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { fetchTopics } from "../api";
 
-export default function Topics(){
-    const[topics,setTopics]=useState([])
-    useEffect(()=>{
-        fetchTopics().then(body=>{
-            setTopics(body.topics)
-        })
-    },[])
+export default function Topics() {
+  const [topics, setTopics] = useState([]);
+  useEffect(() => {
+    fetchTopics().then((body) => {
+      setTopics(body.topics);
+    });
+  }, []);
 
-    return (<div>
-        {topics.map(topic=>{
-         return <div key ={topic.slug}>
-         <Link to={"/articles/"+topic.slug} state={{topic:topic.slug}}> <h1>
-            {topic.slug}</h1>
-            {topic.description} </Link>
-         </div> 
-        })}
- 
-     </div>)
+  return (
+    <div>
+      {topics.map((topic) => {
+        return (
+          <div key={topic.slug}>
+            <Link to={"/articles/" + topic.slug} state={{ topic: topic.slug }}>
+              <h2 className="topic-title">{topic.slug}</h2>
+              {topic.description}{" "}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
