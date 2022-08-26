9e5a12c6ddbb95bb1ce5abde2a5fc0f0247ca1a8
shamelhok <msh_shamel@hotmail.co.uk>
2022-08-26 15:11:25 +0100

    refactor colouring in votes to work properly on comments

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -5,6 +5,8 @@ export default function Votes(article) {
   const [votes, setVotes] = useState(article.votes);
   const { article_id, onArticlePage, comment_id } = article;
   const [votesByUser, setVotesByUser] = useState(0);
+  const[ downvoteColour,setDownvoteColour]= useState('')
+  const[ upvoteColour,setupvoteColour]= useState('')
   useEffect(() => {
     if (article_id || comment_id) {
       setVotes(article.votes + votesByUser);
@@ -12,74 +14,74 @@ export default function Votes(article) {
   }, [votes, article_id, votesByUser, article.votes, comment_id]);
   function upVote(event) {
     if (votesByUser === 0) {
-      event.target.style.backgroundColor = "green";
+      setupvoteColour('green')
       setVotesByUser((current) => current + 1);
       patchVotes(article_id, 1, comment_id).catch((err) => {
         console.log(err);
-        event.target.style.backgroundColor = "";
+        setupvoteColour('')
         setVotesByUser((current) => current - 1);
       });
     } else if (votesByUser > 0) {
-      event.target.style.backgroundColor = "";
+      setupvoteColour('')
       setVotesByUser((current) => current - 1);
       patchVotes(article_id, -1, comment_id).catch((err) => {
         console.log(err);
-        event.target.style.backgroundColor = "green";
+        setupvoteColour('green')
         setVotesByUser((current) => current + 1);
       });
     } else if (votesByUser < 0) {
-      event.target.style.backgroundColor = "green";
-      document.querySelector(".downvote").style.backgroundColor = "";
+      setupvoteColour('green')
+      setDownvoteColour('')
       setVotesByUser((current) => current + 2);
       patchVotes(article_id, 2, comment_id).catch((err) => {
         console.log(err);
-        event.target.style.backgroundColor = "";
-        document.querySelector(".downvote").style.backgroundColor = "red";
+        setupvoteColour('')
+        setDownvoteColour('red')
         setVotesByUser((current) => current - 2);
       });
     }
   }
   function downVote(event) {
     if (votesByUser === 0) {
-      event.target.style.backgroundColor = "red";
+      setDownvoteColour('red')
       setVotesByUser((current) => current - 1);
       patchVotes(article_id, -1, comment_id).catch((err) => {
         console.log(err);
-        event.target.style.backgroundColor = "";
+        setDownvoteColour('')
         setVotesByUser((current) => current + 1);
       });
     } else if (votesByUser < 0) {
       setVotesByUser((current) => current + 1);
-      event.target.style.backgroundColor = "";
+      setDownvoteColour('')
       patchVotes(article_id, +1, comment_id).catch((err) => {
         console.log(err);
-        event.target.style.backgroundColor = "red";
+        setDownvoteColour('red')
         setVotesByUser((current) => current - 1);
       });
     } else if (votesByUser > 0) {
-      event.target.style.backgroundColor = "red";
-      document.querySelector(".upvote").style.backgroundColor = "";
+      setDownvoteColour('red')
+      setupvoteColour('')
       setVotesByUser((current) => current - 2);
       patchVotes(article_id, -2, comment_id).catch((err) => {
         console.log(err);
-        event.target.style.backgroundColor = "";
-        document.querySelector(".upvote").style.backgroundColor = "green";
+        setDownvoteColour('')
+        setupvoteColour('green')
         setVotesByUser((current) => current + 2);
       });
     }
   }
 
   return (
-    <span>
+    <span >
       {onArticlePage ? (
-        <button className="downvote" onClick={downVote}>
+        <button style={{backgroundColor:downvoteColour}} className={"downvote"} onClick={downVote}>
           {" "}
           bad{" "}
         </button>
       ) : null}
       <span className="votes"> {votes} vote(s) </span>
       {onArticlePage ? (
-        <button className="upvote" onClick={upVote}>
+        <button style={{backgroundColor:upvoteColour}} className={"upvote"} onClick={upVote}>
           {" "}
           good{" "}
         </button>
